3d9f13ebf03ef7c8bd4a7279078a0c5453c281a7
TobiasComp <tuvia1234@gmail.com>
2020-09-29 17:36:53 +0300

    static

--- DaoLearning1/DaoLearning1/settings.py
+++ DaoLearning1/DaoLearning1/settings.py
@@ -83,13 +83,25 @@ WSGI_APPLICATION = 'DaoLearning1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': '/etc/mysql/my.cnf',
+#         },
+#         'NAME': 'shopDjango',
+#         'USER': 'root',
+#         'PASSWORD': '1q2w3e4r',
+#         'HOST': 'localhost',
+#         'PORT': 3306
+#     }
+# }
 # tuvias stuff
 # DATABASES = {
 #     'default': {
@@ -147,3 +159,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/tuvia1234/DaoLearning1/static'
